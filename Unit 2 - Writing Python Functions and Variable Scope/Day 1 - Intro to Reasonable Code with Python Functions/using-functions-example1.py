26a82939eb080ed6722dc0443e3aa92c4710d71b
Alex Lee <allee@bayviewglen.ca>
2023-10-18 11:39:42 -0400

    Examples and HW finsihed version

---
+++ Unit 2 - Writing Python Functions and Variable Scope/Day 1 - Intro to Reasonable Code with Python Functions/using-functions-example1.py
@@ -0,0 +1,19 @@
+length = len("Happy Cat")
+
+
+print(length)
+
+
+mystery = print("Happy Cat")
+
+
+print(mystery)   # print returns nothing (None)
+
+
+width = int(input("Please enter a width: "))
+# "Please enter a width " is the argument for the input function
+# it returns what the user entered
+# that will be the argument for the int function
+
+
+print(round(34.3645,  2))
