adf191e90d04bc22db86f7ac2bd02d5891a549e8
Alex Lee <allee@bayviewglen.ca>
2023-10-22 11:11:43 -0400

    Day 2 Work done

---
+++ Unit 2 - Writing Python Functions and Variable Scope/Day 2 - Writing Functions with Parameters/example-three.py
@@ -0,0 +1,29 @@
+def print_greeting(name):
+    print(f"Hello {name}")
+
+def area_of_rectangle(length, width):
+    print(f"The area of the rectangle is: {length*width}")
+
+
+
+
+
+person_name = input("Please enter a name: ")
+print_greeting(person_name)
+
+######
+
+area_of_rectangle(5,8)
+
+######
+
+def print_pattern(char):
+    print(char)
+    print(char)
+    print(char)
+    print(char)
+    print(char)
+    print(char)
+
+
+print_pattern("🥙")
